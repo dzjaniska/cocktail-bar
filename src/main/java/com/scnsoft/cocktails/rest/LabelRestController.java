591a99cd59d7bbcef31c34ddaabfbd68659b1bd7
Tuegor <tuegor@tut.by>
2022-02-14 18:18:38 +0300

    Corrected label update Added label exception handling

--- src/main/java/com/scnsoft/cocktails/rest/LabelRestController.java
+++ src/main/java/com/scnsoft/cocktails/rest/LabelRestController.java
@@ -3,8 +3,14 @@ package com.scnsoft.cocktails.rest;
 import java.util.List;
 import java.util.UUID;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.scnsoft.cocktails.entity.LabelDTO;
 import com.scnsoft.cocktails.facade.LabelFacade;
@@ -42,13 +49,32 @@ public class LabelRestController {
 	
 	@PutMapping
 	public LabelDTO updateLabel(@RequestBody LabelDTO theLabel) {
-		return labelFacade.save(theLabel);
+		LabelDTO theLabelDB = labelFacade.getById(theLabel.getId());
+		theLabelDB.setLabelEn(theLabel.getLabelEn());
+		theLabelDB.setLabelRu(theLabel.getLabelRu());
+		
+		return labelFacade.save(theLabelDB);
 	}
 	
 	@DeleteMapping("/{labelId}")
-	public String deleteLabel(@PathVariable UUID labelId) {
+	public ResponseEntity<String> deleteLabel(@PathVariable UUID labelId) {
 		labelFacade.deleteById(labelId);
 		
-		return "Deleted label id - " + labelId;
+		return new ResponseEntity<>("", HttpStatus.NO_CONTENT);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<String> handleException(EntityNotFoundException exc) {
+		return new ResponseEntity<>(exc.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<String> handleException(EmptyResultDataAccessException exc) {
+		return new ResponseEntity<>(exc.getMessage(), HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<String> handleException(Exception exc) {
+		return new ResponseEntity<>(exc.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
